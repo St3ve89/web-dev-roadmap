896d40806073af4d516d21ecf3d047a9c447e6be
St3ve89 <istvan.acs89@gmail.com>
2018-11-01 06:51:16 +0000

    Make the searchbar working

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -5,6 +5,7 @@ import { robots } from './robots';
 
 
 class App extends Component {
+
   constructor() {
     super()
     this.state = {
@@ -12,12 +13,20 @@ class App extends Component {
       searchfield: ''
     }
   }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
   render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    })
     return (
       <div className="tc">
         <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={this.state.robots}/>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots}/>
       </div>
     )
   }
