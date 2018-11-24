959feba0ea509bfad0f00ff665274da7e822aca3
Istvan Acs <istvan.acs89@gmail.com>
2018-11-24 20:50:27 +0000

    Updates

---
+++ precache-manifest.95820ce13c567aeda31f20f265cec2da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/web-dev-roadmap/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ae07be0b83c373cb27d2",
+    "url": "/web-dev-roadmap/static/js/runtime~main.ae07be0b.js"
+  },
+  {
+    "revision": "67dee0a2be8943ec0054",
+    "url": "/web-dev-roadmap/static/js/main.67dee0a2.chunk.js"
+  },
+  {
+    "revision": "add44d7f253a99da1144",
+    "url": "/web-dev-roadmap/static/js/1.add44d7f.chunk.js"
+  },
+  {
+    "revision": "67dee0a2be8943ec0054",
+    "url": "/web-dev-roadmap/static/css/main.b28750c0.chunk.css"
+  },
+  {
+    "revision": "add44d7f253a99da1144",
+    "url": "/web-dev-roadmap/static/css/1.7a516413.chunk.css"
+  },
+  {
+    "revision": "73f2b4345c75fe1a5719e060c9d70af9",
+    "url": "/web-dev-roadmap/index.html"
+  }
+];
