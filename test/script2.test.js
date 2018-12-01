afa330fe7fadebbd2a69d8488145a467fedbed43
Istvan Acs <istvan.acs89@gmail.com>
2018-12-01 21:23:20 +0000

    Testing fetch

--- test/script2.test.js
+++ test/script2.test.js
@@ -1,3 +1,15 @@
 const fetch = require('node-fetch')
 const swapi = require('./script2')
 
+it('calls swapi to get people', () => {
+    expect.assertions(1)
+    swapi.getPeople(fetch).then(data => {
+        expect(data.count).toEqual(87)
+    })
+})
+
+it('calls swapi to get people with a promise', () => {
+    swapi.getPeoplePromise(fetch).then(data => {
+        expect(data.count).toEqual(87)
+    })
+})
